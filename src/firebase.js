0014db813b278b3a51ef48e70831e707e12d0d2c
stevebraco <64028456+stevebraco@users.noreply.github.com>
2021-05-21 01:56:55 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase/app';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyB6leF4lMFnWkje7G8vymMq-5KgBZi4X00",
-  authDomain: "instagram-clone-react-732e9.firebaseapp.com",
-  projectId: "instagram-clone-react-732e9",
-  storageBucket: "instagram-clone-react-732e9.appspot.com",
-  messagingSenderId: "613301754349",
-  appId: "1:613301754349:web:c9babcefb62ae08651afe4",
-  measurementId: "G-M505CHRYT8"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 });
 
 const db = firebaseApp.firestore();
